050ef11cd07472fdec262f48c7d1a530bcbc79f4
Vladimir Kleymenov <vladimir.kleymenov@rtlabs.ru>
2021-12-10 18:59:43 +0300

    2 работа

--- 2.py
+++ 2.py
@@ -1,8 +1,11 @@
 from control.matlab import tf, c2d, feedback, stepinfo, step, lsim
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
 s = tf('s')
+z = tf('z')
+
 k = 2
 tp = 1
 T = 0.01
@@ -15,28 +18,41 @@ R = ((s + 8) * ((3 / math.pow(6.2, 2) + 5 / math.pow(6.2, 3)) * (s * s) + (3 / 6
 Wn_dis = c2d(Wn, T)
 R_dis = c2d(R, T)
 
-
 Wyg_dis = feedback(R_dis * Wn_dis, 1, -1)
 
 
 #Единичное ступ. воздействие
-yout, T_step = step(Wyg_dis)
+h, T_step = step(Wyg_dis)
 
 info = stepinfo(Wyg_dis, SettlingTimeThreshold=0.05)
 
-print('SettlingTime:', info['SettlingTime'])
-plt.plot(T_step, yout)
+print('SettlingTime: %.4f' % (info['SettlingTime']))
+plt.plot(T_step, h)
 plt.xlabel('t')
-plt.ylabel('yout')
+plt.ylabel('h step')
 plt.show()
 
-# Ошибка ???????????????
+# Ошибка step
 Weg = feedback(1, R_dis * Wn_dis, -1)
 
 e_g, T_e = step(Weg)
 
 plt.plot(T_e, e_g)
 plt.xlabel('t')
-plt.ylabel('e_g')
+plt.ylabel('e_g step')
+plt.show()
+
+
+# Ошибка lsim
+t = np.arange(0, 1.1, 0.1)
+print(t)
+g = 2*t + 1
+print('g', g)
+e_g_lsim = lsim(Weg, g, t)
+e_g_yout = e_g_lsim[0]
+e_g_time = e_g_lsim[1]
+plt.plot(e_g_time, e_g_yout)
+plt.xlabel('t')
+plt.ylabel('e_g lsim')
 plt.show()
 
